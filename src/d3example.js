5df3fcf449e78065913d4fc619764acaa64f6346
Kevin Herrarte <kevinfertech@gmail.com>
2019-04-22 12:04:22 -0600

    Added basic test using chart component and d3

--- src/d3example.js
+++ src/d3example.js
@@ -2,7 +2,53 @@ require("aframe");
 const d3 = require("d3");
 window.onload = function () {
   function generateData() {
-    return [{"label":"Apples","x":9,"z":1,"y":22,"customer":"Store A","size":1,"color":"yellow"},{"label":"Bananas","x":4,"z":2,"y":25,"customer":"Store A","size":1,"color":"yellow"},{"label":"Cantaloupe","x":9,"z":9,"y":16,"customer":"Store A","size":1,"color":"yellow"},{"label":"Dates","x":6,"z":7,"y":2,"customer":"Store A","size":1,"color":"red"},{"label":"Grapes","x":4,"z":7,"y":22,"customer":"Store A","size":1,"color":"yellow"}]
+    return [
+      {
+          "label": "Apples",
+          "x": 9,
+          "z": 1,
+          "y": 22,
+          "customer": "Store A",
+          "size": 1,
+          "color": "red"
+      },
+      {
+          "label": "Bananas",
+          "x": 4,
+          "z": 2,
+          "y": 25,
+          "customer": "Store A",
+          "size": 1,
+          "color": "yellow"
+      },
+      {
+          "label": "Cantaloupe",
+          "x": 9,
+          "z": 9,
+          "y": 16,
+          "customer": "Store A",
+          "size": 1,
+          "color": "orange"
+      },
+      {
+          "label": "Dates",
+          "x": 6,
+          "z": 7,
+          "y": 2,
+          "customer": "Store A",
+          "size": 1,
+          "color": "green"
+      },
+      {
+          "label": "Grapes",
+          "x": 4,
+          "z": 7,
+          "y": 22,
+          "customer": "Store A",
+          "size": 1,
+          "color": "blue"
+      }
+  ]
   }
 
   function generateBars(scale) {
@@ -30,21 +76,7 @@ window.onload = function () {
       .attr('position', d => `0 ${scale(d.y)/2+.5} 0`)
       .attr('scale', '1 1 1')
       .attr("value", d => `${d.label}: ${d.y}`);
-  }
-
-  function showLabel(bar, d) {
-    bar.select("a-text")
-      .attr('color', 'black')
-	    .attr('align', 'center')
-	    .attr('position', `0 ${d.y/2+.5} 0`)
-	    .attr('scale', '1 1 1')
-      .attr("value", `${d.label}: ${d.y}`);
-  }
-
-  function hideLabel(bar) {
-    bar.select("a-text")
-      .attr("scale", ".01 .01 .01");
-  }
+  } 
 
   var scene = d3.select("a-scene");
   var data = generateData();
